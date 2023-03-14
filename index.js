b24c3f50d3ca3daaaa93c6ebcdf024ca46af7279
Mbiki-KK <wawerumbiki@gmail.com>
2023-03-14 16:23:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(initialBlock, block2) {
+    console.log(initialBlock-block2)
+}
+distanceFromHqInBlocks(42,72)
+
+function distanceFromHqInFeet(initialBlock,block2){
+    return((initialBlock-block2)*264)
+}
+distanceFromHqInFeet(42,64)
